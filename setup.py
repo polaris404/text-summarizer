6bcdd7978535cb23d088f4b4388f036200c6d7b0
polaris404 <mohitsoni.01@outlook.com>
2024-05-24 13:11:24 +0530

    Add requirements

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer"
+AUTHOR_USER_NAME = "polaris404"
+SRC_REPO = "text-summarizer"
+AUTHOR_EMAIL = "mohitsoni.01@outlook.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Text Summarizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    package_dir={"": "text-summarizer"},
+    packages=setuptools.find_packages(where='text-summarizer')
+)
